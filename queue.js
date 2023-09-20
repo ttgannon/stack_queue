efef6f41009c99e39fd298f154300f1385fc03e4
ttgannon <ga1179@nyu.edu>
2023-09-20 12:17:24 -0300

    queue and stack

---
+++ queue.js
@@ -0,0 +1,70 @@
+/** Node: node for a queue. */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+/** Queue: chained-together nodes where you can
+ *  remove from the front or add to the back. */
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  /** enqueue(val): add new value to end of the queue. Returns undefined. */
+
+  enqueue(val) {
+    const newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.size++;
+  }
+
+  /** dequeue(): remove the node from the start of the queue
+   * and return its value. Should throw an error if the queue is empty. */
+
+  dequeue() {
+    if (!this.first) {
+      throw new Error("This list is empty.")
+    } 
+    const removedNode = this.first;
+    if (this.size === 1) {
+      this.first = null;
+      this.last = null;
+    } else {
+      this.first = this.first.next;
+    }
+    this.size--;
+    return removedNode.val;
+  }
+
+  /** peek(): return the value of the first node in the queue. */
+
+  peek() {
+    return this.first.val;
+  }
+
+  /** isEmpty(): return true if the queue is empty, otherwise false */
+
+  isEmpty() {
+    if (!this.first) {
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+}
+
+module.exports = Queue;
